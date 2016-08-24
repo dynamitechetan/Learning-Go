7a5a2ea1a0f54c30aa104da3bdccf19434188576
Chetan Kaushik <dynamitechetan@gmail.com>
2016-08-24 20:55:33 +0530

    Create Variables.go

---
+++ Variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+func main() {
+    //trying a String
+    var a string = "some strings here"
+    fmt.Println(a)
+    //trying a integer 
+    var var1, var2 int = 1, 2
+    fmt.Println(var1, var2)
+    //trying a boolean 
+    var some_boolean = true
+    fmt.Println(some_boolean) 
+}
