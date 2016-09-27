902ec9858d8905edb78fe1034737eb1cb08e6f79
Chetan Kaushik <dynamitechetan@gmail.com>
2016-09-27 13:11:48 +0530

    Create Channel_Buffering.go

---
+++ Channel_Buffering.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+// channel of strings buffering up to 2 values.
+messages := make(chan string, 2)
+
+messages <- "buffered"
+messages <- "channel"
+	
+  // retreival of values
+  fmt.Println(<-messages)
+  fmt.Println(<-messages)
+}
