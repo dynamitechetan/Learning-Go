094d9fc9d1e55784c2a13bfe24e7acf1369a6919
Chetan Kaushik <dynamitechetan@gmail.com>
2016-10-01 22:43:41 +0530

    Create timers.go

---
+++ timers.go
@@ -0,0 +1,16 @@
+package main
+
+import "time"
+import "fmt"	
+
+func main() {
+// This timer will wait 2 seconds.
+	
+
+    timer1 := time.NewTimer(time.Second * 2)
+	
+    <-timer1.C
+    fmt.Println("Timer 1 expired")
+
+
+}
