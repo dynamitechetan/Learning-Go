81fdf07757072d7d87b2f6c5fd640cb04cf097e0
Chetan Kaushik <dynamitechetan@gmail.com>
2016-12-19 15:01:25 +0530

    Create Log.go

---
+++ Log.go
@@ -0,0 +1,9 @@
+package main
+        import (
+        	"log"
+        )
+        func main() {
+        	log.SetFlags(log.Ltime | log.Lshortfile)
+        	log.Println("first log output")
+        	log.Printf("second log output\n")
+ }
